eb6ea90369a025808b6d3dae87cf94f1a8c53946
Raunak Singh <rksj2001@gmail.com>
2022-04-19 18:04:28 +0530

    added chart in /coin

--- client/src/pages/Coin.jsx
+++ client/src/pages/Coin.jsx
@@ -4,16 +4,45 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/system';
 import getCoinApi from '../api/getCoin';
 import { Avatar, Typography, Grid, Card, Divider, CardContent, CardHeader, Container, Link } from '@mui/material';
+import { Line } from 'react-chartjs-2';
+import {Chart as ChartJS} from 'chart.js/auto';
+
+// const chartData = {
+//   labels: [1, 2],
+//   datasets: [{
+//     label: "Users Gained",
+//     data: [100, 200],
+//     backgroundColor: "blue"
+//   }]
+// }
 
 const Coin = () => {
   const { CoinUUID } = useParams();
   const [coin, setCoin] = useState(null);
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [{
+      label: "",
+      data: []
+    }]
+  })
+  console.log(chartData)
 
   useEffect(() => {
     const getCoin = async () => {
       const response = await getCoinApi.get("/" + CoinUUID);
       setCoin(response?.data?.coin);
-      console.log(response?.data?.coin?.links);
+      console.log(response?.data?.coin?.sparkline);
+      const sparklineData = response?.data?.coin?.sparkline;
+      const lineData = {
+        labels: sparklineData.map((value, id) => (id)),
+        datasets: [{
+          label: "Price",
+          data: sparklineData.map((value, id) => (parseFloat(value))),
+          backgroundColor: "blue"
+        }]
+      }
+      setChartData(lineData);
     };
     try {
       getCoin();
@@ -36,11 +65,20 @@ const Coin = () => {
             />
             <Divider />
             <CardContent>
-              <Typography variant="subtitle1">Rank: ${coin.rank}</Typography>
-              <Typography variant="subtitle1">Price: ${parseFloat(coin.price).toFixed(2)}</Typography>
-              <Typography variant="subtitle1">MarketCap: ${millify(coin.marketCap)}</Typography>
-              <Typography variant="subtitle1">Current Volume: ${millify(coin["24hVolume"])}</Typography>
-              <Link underline="hover" variant="subtitle1" href={coin.websiteUrl}>Website Link</Link>
+              <Grid container spacing={0}>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={3}>
+                  <Typography variant="subtitle1">Rank: ${coin.rank}</Typography>
+                  <Typography variant="subtitle1">Price: ${parseFloat(coin.price).toFixed(2)}</Typography>
+                  <Typography variant="subtitle1">MarketCap: ${millify(coin.marketCap)}</Typography>
+                  <Typography variant="subtitle1">Current Volume: ${millify(coin["24hVolume"])}</Typography>
+                  <Link underline="hover" variant="subtitle1" href={coin.websiteUrl}>Website Link</Link>
+                </Grid>
+                <Grid item xs={7}>
+                  <Line data={chartData} />
+                </Grid>
+                <Grid item xs={1}></Grid>
+              </Grid>
               <br />
               <Divider />
               {/* We are getting html elements from the backend in the description
