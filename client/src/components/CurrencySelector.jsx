19f057d2fd45ed59a2402a9689cbdfbab66de316
Raunak Singh <rksj2001@gmail.com>
2022-06-27 22:48:45 +0530

    Created currency selector

--- client/src/components/CurrencySelector.jsx
+++ client/src/components/CurrencySelector.jsx
@@ -1,44 +1,60 @@
-import React from 'react';
-import { Button, Box } from '@mui/material';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Button, Box, Divider } from '@mui/material';
+import { ClickAwayListener } from '@mui/base';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import currencyList from "../assets/currency-list.json";
 
 const CurrencySelector = () => {
   const [selectHover, setSelectHover] = useState(false);
+  const [currency, setCurrency] = useState("USD")
+
+  const handleClickAway = () => {
+    setSelectHover(false);
+  }
+
+  const handleSelect = (curr) => {
+    setCurrency(curr);
+    setSelectHover(false);
+    console.log(selectHover);
+  }
+
+  const handleOptionsOpen = () => {
+    if (selectHover === false)
+      setSelectHover(true);
+  }
 
   return (
-    <Button variant="outlined" size="medium" color="inherit">
-      <div class="dropdown" style={{
-        position: "relative",
-        display: "inline-block"
-      }}
-        onMouseOver={() => setSelectHover(true)}
-        onMouseOut={() => setSelectHover(false)}
-      >
-        {/* <Box style={{ margin: "0px", padding: "0px", display: "flex", justifyContent: "left" }}> */}
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <Button onClick={() => {handleOptionsOpen()}} variant="outlined" size="medium" color="inherit">
         <Box style={{
           margin: "0px",
           padding: "0px",
           display: "flex",
-          justifyContent: "left"
+          justifyContent: "center"
         }}>
-          USD
+          {currency}
           <ArrowDropDownIcon style={{ height: "20px" }} />
         </Box>
         <div class="dropdown-content" style={{
           display: selectHover ? "block" : "none",
           position: "absolute",
           backgroundColor: "#f9f9f9",
-          boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.2)",
-          padding: "12px 16px",
-          zIndex: 100,
+          width: "90px",
+          zIndex: 1,
           color: "black",
+          maxHeight: "80px",
+          overflow: "scroll",
+          borderRadius: "4px"
         }}>
-          <Button color="secondary">USD</Button>
-          <Button color="secondary">INR</Button>
+          <Button onClick={() => {handleSelect("INR")}} sx={{ width: "90px" }} color="secondary">INR</Button>
+          <Button onClick={() => {handleSelect("USD")}} sx={{ width: "90px" }} color="secondary">USD</Button>
+          <Divider />
+          {Object.keys(currencyList).map(curr => (
+            <Button onClick={() => {handleSelect(curr)}} key={curr} sx={{ width: "90px" }} color="secondary">{curr}</Button>
+          ))}
         </div>
-      </div>
-    </Button>
+      </Button>
+    </ClickAwayListener>
   )
 }
 
